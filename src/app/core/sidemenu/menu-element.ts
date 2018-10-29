9843fa0ff0a7ed63715f61637f987a3e9041d2e5
ArguellesMA <marco_arguelles97@hotmail.com>
2018-10-29 16:59:31 -0600

    cdk facturacion

---
+++ src/app/core/sidemenu/menu-element.ts
@@ -0,0 +1,417 @@
+export const menus = [
+    {
+        'name': 'Documentos',
+        'icon': 'dashboard',
+        'link': false,
+        'open': false,
+        'chip': { 'value': 1, 'color': 'accent' },
+        'sub': [
+            {
+                'name': 'Generar CFDI 3.3',
+                'link': '/auth/dashboard',
+                'icon': 'dashboard',
+                'chip': false,
+                'open': true,
+            },
+            {
+                'name': 'Generar nomina 1.2',
+                'link': '/auth/dashboard',
+                'icon': 'list',
+                'chip': false,
+                'open': true,
+            },
+            {
+                'name': 'Generar complemento pagos',
+                'link': '/auth/dashboard',
+                'icon': 'dashboard',
+                'chip': false,
+                'open': true,
+            },
+            {
+                'name': 'Generar retenciones',
+                'link': '/auth/dashboard',
+                'icon': 'dashboard',
+                'chip': false,
+                'opne': true,
+            },
+            {
+                'name': 'Generar comercio exterior 1.1',
+                'link': '/auth/dashboard',
+                'icon': 'dashboard',
+                'chip': false,
+                'open': true,
+            }
+        ]
+    },
+    {
+        'name': 'Catalogos',
+        'icon': 'widgets',
+        'link': false,
+        'open': false,
+        'sub': [
+            {
+                'name': 'Clientes',
+                'link': 'material-widgets/buttons',
+                'icon': 'indeterminate_check_box',
+                'chip': false,
+                'open': false,
+            },
+            {
+                'name': 'Empleados',
+                'link': 'material-widgets/list',
+                'icon': 'list',
+                'chip': false,
+                'open': false,
+            },
+            {
+
+                'name': 'Etiquetas personalizadas',
+                'link': 'material-widgets/stepper',
+                'icon': 'view_week',
+                'chip': false,
+                'open': false,
+
+            },
+            {
+                'name': 'Impuestos',
+                'link': 'material-widgets/expansion',
+                'icon': 'web_aaset',
+                'chip': false,
+                'open': false,
+            },
+            {
+                'name': 'Impuestos locales',
+                'link': 'material-widgets/spinner',
+                'icon': 'cached',
+                'chip': false,
+                'open': false,
+            },
+            {
+                'name': 'Mis plantillas PDF',
+                'link': 'material-widgets/cards',
+                'icon': 'crop_16_9',
+                'chip': false,
+                'open': false,
+            },
+            {
+                'name': 'Productos',
+                'link': 'material-widgets/icons',
+                'icon': 'gif',
+                'chip': false,
+                'open': false,
+            },
+          //  {
+          //      'name': '..........',
+          //      'link': 'material-widgets/autocomplete',
+          //      'icon': 'get_app',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '.........',
+          //      'link': 'material-widgets/checkbox',
+          //      'icon': 'check_box',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '.........',
+          //      'link': 'material-widgets/datepicker',
+          //      'icon': 'date_range',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '..........',
+          //      'link': 'material-widgets/slider',
+          //      'icon': 'keyboard_tab',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '..........',
+          //      'link': 'material-widgets/slide-toggle',
+          //      'icon': 'album',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '..........',
+          //      'icon': 'menu',
+          //      'link': 'material-widgets/menu',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '..........',
+          //      'link': 'material-widgets/progress-bar',
+          //      'icon': 'trending_flat',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '..........',
+          //      'icon': 'input',
+          //      'link': 'material-widgets/input',
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '.........',
+          //      'icon': 'radio_button_checked',
+          //      'link': 'material-widgets/radio',
+          //      'chip': false,
+          //      'open': false,
+          //  },
+          //  {
+          //      'name': '..........',
+          //      'icon': 'select_all',
+          //      'link': 'material-widgets/select',
+          //      'open': false,
+          //  },
+        ]
+    },
+    // {
+    //     'name'   : '..........',
+    //     'icon'   : 'mode_edit',
+    //     'open'   : false,
+    //     'link'   : false,
+    //     'sub'    :  [
+    //                     {
+    //                         'name': 'Template Driven',
+    //                         'icon': 'mode_edit',
+    //                         'open'   : false,
+    //                         'link':'forms/template_forms'
+    //                     },
+    //                     {
+    //                         'name': 'Reactive Forms',
+    //                         'icon': 'text_fields',
+    //                         'open'   : false,
+    //                         'link':'forms/reactive_forms'
+    //                     }
+    //                 ]
+    // },
+    {
+        'name': 'Herramientas',
+        'icon': 'list',
+        'link': false,
+        'open': false,
+        'chip': { 'value': 2, 'color': 'accent' },
+        'sub': [
+            {
+                'name': 'Bridge TXT 3.3',
+                'icon': 'filter_list',
+                'link': 'tables/fixed',
+                'open': false,
+            },
+            {
+                'name': 'Bridge TXT 3.2',
+                'icon': 'done_all',
+                'link': 'tables/featured',
+                'open': false,
+            },
+            {
+                'name': 'Buzon',
+                'icon': 'filter_center_focus',
+                'link': 'tables/responsive',
+                'open': false,
+            },
+            {
+                'name': 'Prefacturas',
+                'icon': 'list',
+                'link': 'tables/responsive',
+                'open': false,
+            },
+            {
+                'name': 'Pre-nomina 3.3',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Pre-nomina3.2',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Tickets',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            }
+        ]
+
+    },
+    {
+        'name': 'Registros',
+        'icon': 'mode_edit',
+        'link': false,
+        'open': false,
+        'sub': [
+            {
+                'name': 'CFDI 3.3',
+                'icon': 'dashboard',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Retenciones',
+                'icon': 'list',
+                'link':  '',
+                'open':  false,
+            }
+        ]
+    }, 
+    //{
+    //    'name': 'Scrumboard',
+    //    'open': false,
+    //    'link': '/auth/scrumboard',
+    //    'icon': 'grade',
+    //},
+    //{
+    //    'name': 'Applications',
+    //    'icon': 'view_module',
+    //    'open': false,
+    //    'link': false,
+    //    'sub': [
+    //        {
+    //            'name': 'chat',
+    //            'icon': 'chat',
+    //            'link': 'chats/chat',
+    //            'open': false,
+    //        },
+    //        {
+    //            'name': 'mail',
+    //            'icon': 'mail',
+    //            'link': 'mail/mail',
+    //            'open': false,
+    //        },
+    //        {
+    //            'name': 'Editor',
+    //            'icon': 'editor',
+    //            'link': 'editor/editor',
+    //            'open': false,
+    //        }
+    //    ]
+    //},
+     {
+        'name': 'Configuracion de cuenta',
+        'icon': 'content_copy',
+        'open': false,
+        'link': false,
+        'sub': [
+            {
+                'name': 'Informacion fiscal',
+                'icon': 'work',
+                'open': false,
+                'link': '../login',
+            }, 
+            {
+                'name': 'Sucursales/Cuentas',
+                'icon': 'local_laundry_service',
+                'open': false,
+                'link': 'pages/services',
+            },
+            {
+                'name': 'Certificado',
+                'icon': 'directions',
+                'open': false,
+                'link': 'pages/contact'
+            },
+            {
+                'name': 'Activacion de creditos de CFDI',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Activacion de creditos de recepcion',
+                'icon': 'dashboard',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Logos',
+                'icon': 'work',
+                'link': '',
+                'open': false,  
+            },
+            {
+                'name': 'Activar complementos',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Mis pagos',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Correo',
+                'icon': 'work',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Carta manifiesto',
+                'icon': 'dashboard',
+                'link': '',
+                'open': false,
+            },
+            {
+                'name': 'Configuracion general',
+                'icon': 'list',
+                'link': '',
+                'open': false,
+            }
+        ]
+    },
+    //{
+    //    'name': '..........',
+    //    'icon': 'pie_chart_outlined',
+    //    'open': false,
+    //    'link': false,
+    //    'sub': [
+    //        {
+    //            'name': 'chartjs',
+    //            'icon': 'view_list',
+    //            'link': 'charts/chartjs',
+    //            'open': false,
+    //        },
+    //        {
+    //            'name': 'ngx-chart',
+    //            'icon': 'show_chart',
+    //            'open': false,
+    //            'link': 'charts/ngx-charts',
+    //        },
+    //        {
+    //            'name': 'nvd3',
+    //            'icon': 'pie_chart',
+    //            'open': false,
+    //            'link': 'charts/nvd3-charts',
+    //        }
+    //    ]
+    //}, 
+    //{
+    //    'name': '..........',
+    //    'icon': 'map',
+    //    'open': false,
+    //    'link': false,
+    //    'sub': [
+    //        {
+    //            'name': 'google-map',
+    //            'icon': 'directions',
+    //            'link': 'maps/googlemap',
+    //            'open': false,
+    //        },
+    //       {
+    //            'name': 'leaflet-map',
+    //            'icon': 'directions',
+    //            'link': 'maps/leafletmap',
+    //            'open': false,
+    //        }
+    //    ]
+    //}
+];
