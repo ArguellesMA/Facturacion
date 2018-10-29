324c85258b2e3486cc2841a08ef1559c6e731251
ArguellesMA <marco_arguelles97@hotmail.com>
2018-10-29 16:51:26 -0600

    hi

--- src/app/models/book.model.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-    constructor(
-        public id: number,
-        public dproducto: string,
-        public servicio: string,
-        public descripcion: string
-    ){}
-}
